73cef0a3ee13b9364b7293e92f5edf0b0649b7ba
w <756424501@qq.com>
2019-08-11 14:30:10 +0800

    repush_2

---
+++ HomeWork/Lesson7/ZZandYamataNoOrochi/Assets/wupeng/Scripts/GameLogic.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public enum TurnRound {dashe, firebird, zz}
+
+    public enum GameState { waiting, gameing, end}
+
+    public TurnRound turn = TurnRound.firebird;
+
+    public GameState gameState = GameState.waiting;
+
+    public float timer = 3.0f;
+
+    public float zzTimer = 5.0f;
+
+    public bool isZzAttack = false;
+
+    public GameObject dashe;
+
+    public GameObject zz;
+
+    public GameObject fireBird;
+
+
+    public GameObject wuwu;
+    public GameObject wupeng;
+    public GameObject baibai;
+    public GameObject mengmeng;
+    public GameObject suye;
+    public GameObject hongyi;
+    public GameObject naomi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsZZAttack();
+        BattleDasheandBird();
+    }
+
+    void BattleDasheandBird()
+    {
+        switch (gameState)
+        {
+            case GameState.end:
+                Wa();
+                break;
+            case GameState.waiting:
+                timer -= Time.deltaTime;
+
+                if (timer < 0.0f)
+                {
+                    gameState = GameState.gameing;
+                    timer = 3.0f;
+                }
+                break;
+            case GameState.gameing:
+
+                IsZZAttack();
+                if (isZzAttack)
+                {
+                    turn = TurnRound.zz;
+                    isZzAttack = false;
+                }
+
+                switch (turn)
+                {
+                    case TurnRound.dashe:
+                        dashe.GetComponent<Charactor>().Attack();
+                        turn = TurnRound.firebird;
+                        gameState = GameState.waiting;
+                        break;
+                    case TurnRound.firebird:
+                        fireBird.GetComponent<Charactor>().Attack();
+                        turn = TurnRound.dashe;
+                        gameState = GameState.waiting;
+                        break;
+                    case TurnRound.zz:
+                        ZZBattle();
+                        break;
+                }
+                break;
+             
+        }
+    }
+
+    void IsZZAttack()
+    {
+        if (fireBird.transform.GetComponent<Charactor>().hp < 500)
+        {
+            isZzAttack = true;
+        }
+    }
+
+    void ZZBattle()
+    {
+        zzTimer -= Time.deltaTime;
+        if (zzTimer < 0.0f)
+        {
+            gameState = GameState.end;
+            Debug.Log("zz打击完毕");
+        }
+    }
+
+    private void Wa()
+    {
+        wuwu.transform.Find("wa").GetComponent<UIBase>().Show("wa",Color.white);
+        wupeng.transform.Find("wa").GetComponent<UIBase>().Show("wa", Color.white);
+        baibai.transform.Find("wa").GetComponent<UIBase>().Show("wa", Color.white);
+        mengmeng.transform.Find("wa").GetComponent<UIBase>().Show("wa", Color.white);
+        suye.transform.Find("wa").GetComponent<UIBase>().Show("wa", Color.white);
+        hongyi.transform.Find("wa").GetComponent<UIBase>().Show("wa", Color.white);
+        naomi.transform.Find("wa").GetComponent<UIBase>().Show("wa", Color.white);
+    }
+}
---
+++ HomeWork/Lesson7/ZZandYamataNoOrochi/Assets/wupeng/Scripts/Skill.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum SkillState { off, on, attacked }
+
+public class Skill : MonoBehaviour
+{
+    public string name_Skill;
+
+    public GameObject owner;
+    public float timer;//重定位时间间隔
+    public float speed;
+    public Vector3 orient;
+
+    public bool isAttaked = false;//技能是否打击到
+
+    public SkillState state = SkillState.off;
+
+    public Skill(GameObject owner)
+    {
+        this.owner = owner;
+        this.state = SkillState.off;
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case SkillState.off:
+                break;
+            case SkillState.on:
+                SkillMove();
+                break;
+            case SkillState.attacked:
+                SkillDestroy();
+                break;
+        }
+    }
+
+    void SkillMove()
+    {
+
+        orient = Vector3.Normalize(owner.GetComponent<Charactor>().target.transform.position - this.transform.position);
+
+        this.GetComponent<RectTransform>().position += orient * speed;
+
+
+        if (Mathf.Abs (this.transform.position.x - owner.GetComponent<Charactor>().target.transform.position.x )< 1
+            && Mathf.Abs( this.transform.position.y - owner.GetComponent<Charactor>().target.transform.position.y) < 1)
+        {
+            state = SkillState.attacked;
+            Debug.Log("击中");
+        }
+    }
+
+    public void SkillDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public void Shoot()
+    {
+        this.state = SkillState.on;
+    }
+}
