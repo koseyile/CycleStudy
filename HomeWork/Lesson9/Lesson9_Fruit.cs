b20cf669cce1871803e427db96e9ffcc2b4e5ad9
mio <amber.wsm@gmail.com>
2019-08-21 15:00:17 +0800

    L9 HW baibai

---
+++ HomeWork/Lesson9/Lesson9_Fruit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit 
+{
+    public string name;
+    public string color;
+    public float weight; // unit: 斤
+
+    public Fruit(string n, string c, float w)
+    {
+        name = n;
+        color = c;
+        weight = w;
+    }
+}
+
+public class Apple: Fruit {
+
+    public Apple(string n, string c, float w) : base(n, c, w) {
+    }
+}
+
+
+
