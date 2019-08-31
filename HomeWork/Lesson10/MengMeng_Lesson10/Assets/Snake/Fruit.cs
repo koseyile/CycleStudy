0942557f3b841e7ca3b21a55d10a72e817d6b191
melfless <mengm@gmail.com>
2019-08-31 12:42:08 +0800

    还没写完的作业

---
+++ HomeWork/Lesson10/MengMeng_Lesson10/Assets/Snake/Fruit.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace MengMeng
+{
+    //[ScriptableObject]
+    public class Fruit : ScriptableObject
+    {
+        public string Name;
+        private string colorname;
+        private Vector3 pos;
+        private GameObject FruitObj;
+
+        public Fruit(string _name, string _colorname, Vector3 _pos, GameObject _Object)
+        {
+            Name = _name;
+            colorname = _colorname;
+            pos = _pos;
+            FruitObj = GameObject.Instantiate(_Object, _pos, Quaternion.identity);
+            MeshRenderer renderer = FruitObj.GetComponent<MeshRenderer>();
+            if (renderer != null)
+            {
+                switch (colorname)
+                {
+                    case "colorful":
+                        renderer.sharedMaterial.color = new Color32(214, 213, 27, 1);
+                        break;
+                    case "red":
+                        renderer.sharedMaterial.color = new Color(0.5f, 0, 0);
+                        break;
+                    case "yellow":
+                        renderer.sharedMaterial.color = new Color(0.5f, 0, 0.5f);
+                        break;
+                    case "green":
+                        renderer.sharedMaterial.color = new Color(0, 0, 0.5f);
+                        break;
+                    case "pink":
+                        renderer.sharedMaterial.color = new Color32(214, 21, 179,1);
+                        break;
+                    default:
+                        renderer.sharedMaterial.color = Color.gray;
+                        break;
+
+                }
+            }
+        }
+
+        public void DestroyFruit()
+        {
+            Destroy(FruitObj);
+        }
+    }
+}
---
+++ HomeWork/Lesson10/MengMeng_Lesson10/Assets/Snake/Snake.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace MengMeng
+{
+    public class SnakeNode
+    {
+        private GameObject snakeObj;
+        private float speed;
+        private Vector3 dir;
+        private float passTime;
+        private Color Bodycolor;
+
+
+        private SnakeNode nextNode;
+
+        public SnakeNode(GameObject _snakePrefab, Vector3 _pos, Color _bodycolor)
+        {
+            speed = 1.0f;
+            dir = new Vector3(-1, 0, 0);
+            Bodycolor = _bodycolor;
+            snakeObj = GameObject.Instantiate(_snakePrefab, _pos, Quaternion.identity);
+            MeshRenderer renderer = snakeObj.GetComponent<MeshRenderer>();
+            renderer.sharedMaterial.color = _bodycolor;
+        }
+
+        public void Update()
+        {
+            passTime += Time.deltaTime;
+            if (passTime > 0.1f)
+            {
+                passTime = 0.0f;
+
+                Move(nextNode, snakeObj.transform.position);
+                Vector3 newPos = snakeObj.transform.position + (dir * speed);
+
+                if (newPos.x > SnakeGame.xMax)
+                {
+                    newPos.x = SnakeGame.xMax;
+                }
+
+                if (newPos.x < -SnakeGame.xMax)
+                {
+                    newPos.x = -SnakeGame.xMax;
+                }
+
+                if (newPos.z > SnakeGame.zMax)
+                {
+                    newPos.z = SnakeGame.zMax;
+                }
+
+                if (newPos.z < -SnakeGame.zMax)
+                {
+                    newPos.z = -SnakeGame.zMax;
+                }
+
+                snakeObj.transform.position = newPos;
+            }
+
+        }
+
+        public bool Move(SnakeNode node, Vector3 _pos)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.snakeObj.transform.position == _pos)
+            {
+                return false;
+            }
+            Move(node.nextNode, node.snakeObj.transform.position);
+            node.snakeObj.transform.position = _pos;
+            return true;
+        }
+
+        public void UpdateInput()
+        {
+            //if(Input.GetKeyUp(KeyCode.Space))
+            //{
+            //    eatFood(this);
+            //}
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                dir = new Vector3(-1, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                dir = new Vector3(1, 0, 0);
+            }
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                dir = new Vector3(0, 0, 1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                dir = new Vector3(0, 0, -1);
+            }
+
+        }
+
+        public void Check()
+        {
+            //if(snakeObj.GetComponent<MeshCollider>().)
+        }
+
+        public void eatFood(SnakeNode node, Fruit fruit = null)
+        {
+            if (fruit)
+            {
+                fruit.DestroyFruit();
+            }
+            Grow(Bodycolor);
+        }
+
+        public void Grow(Color _bodycolor)
+        {
+            if (this.nextNode == null)
+                this.nextNode = new SnakeNode(snakeObj, snakeObj.transform.position, _bodycolor);
+            else
+                this.nextNode.Grow(_bodycolor);
+        }
+    }
+}
