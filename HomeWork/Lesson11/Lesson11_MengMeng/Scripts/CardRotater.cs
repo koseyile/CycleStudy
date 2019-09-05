e09679ade77f5a5f382a405f3291e2989a9283cf
melfless <melfless@foxmail.com>
2019-09-05 23:37:40 +0800

    meng第11课没写完的作业

---
+++ HomeWork/Lesson11/Lesson11_MengMeng/Scripts/CardRotater.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MengMeng
+{
+    public class CardRotater : MonoBehaviour
+    {
+        GameObject Card;
+        public float RotationSpeed;
+        bool bRotateState = false;
+        public bool RotateState { get { return bRotateState; } set { bRotateState = value; } }
+        
+
+        // Use this for initialization
+        void Start()
+        {
+            Card = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Card"),this.transform);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+           if(bRotateState)
+            {
+                RotateCard();
+            }
+        }
+
+        void RotateCard()
+        {
+            Card.transform.Rotate(new Vector3(RotationSpeed, 0, 0));
+        }
+
+        
+    }
+}
+
---
+++ HomeWork/Lesson11/Lesson11_MengMeng/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace MengMeng
+{
+    public class UIManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+       public void OnClickButton_RotateCard()
+        {
+            Transform CardManager = transform.Find("/CardManager");
+            CardRotater cardRotater = CardManager.GetComponentInChildren<CardRotater>();
+            cardRotater.RotateState = !cardRotater.RotateState;
+        }
+    }
+}
+
