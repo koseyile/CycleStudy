fd4c7f81a01f214c2c901a540534fcbf97de3702
eliza <1545834745@qq.com>
2019-09-10 01:46:13 +0800

    L11 HW baibai

--- HomeWork/Lesson11/L11_baibai/Assets/L11HW/BillBoardControl.cs
+++ HomeWork/Lesson11/L11_baibai/Assets/L11HW/BillBoardControl.cs
@@ -6,67 +6,88 @@ using BAIBAI11;
 public class BillBoardControl : MonoBehaviour
 {
     private BAIBAI11.BoardCell unit;
-    private BAIBAI11.TimeCell[] timeUnits;
+    private BAIBAI11.TimeChainCell[] timeChainUnits;
     private BAIBAI11.RanSpeedCell[] ranUnits;
+    private BAIBAI11.BillboardSim[] billUnits;
 
     [SerializeField]
     private string cellPath = "Prefab/UnitCell";
     GameObject cellPrefab;
 
-    bool rotateTest = false;
-
     Camera main;
 
-    private float timer;
-    private float minSec;
-    private float tenMinSec;
-    private float sec;
-    private float tenSec;
-    private float min;
-    private float tenMin;
-    private float hour;
-    private float tenHour;
+    bool rotateTest = false;
+    bool timerOn = false;
+    bool waveOn = false;
 
     // Start is called before the first frame update
     void Start()
     {
         main = FindObjectOfType<Camera>();
 
+
         cellPrefab = Resources.Load(cellPath) as GameObject;
-        unit = new BAIBAI11.BoardCell(cellPrefab, new Vector3(0,0,0),1f);
-        unit.UpdateFace("9", "7");
-        //Debug.Log("w,h: " + unit.Width + ", " + unit.Height);
-
-
-        //7.实例化8个B，分别代表小时，分钟，秒，毫秒（为方便制作，我们定义1秒=60毫秒） WIP
-        CreateTimeCells();
-
-
-        //Q9.实例化600个C，在屏幕上组成一个大矩形，所有卡面使用随机速度翻转。
-        CreateRandomSpeedCells(20,30);
+        CreateUnit();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rotateTest)
+        if (rotateTest && unit != null)
         {
             unit.CellRotate();
-        }
-
-        if(timeUnits != null) {
-            RunTimeCells();
         }
 
         if (ranUnits != null) {
             RotateRandomSpeedCells();
         }
 
+        if (timerOn && timeChainUnits != null) {
+            UpdateTimeChainCells();
+        }
+
+        if(waveOn == false && billUnits != null) {
+            RotateBillBoardCells();
+        }
+
+        if(waveOn && billUnits != null) {
+            WaveBillBoardCells();
+        }
     }
+
+
+    // Q1-Q3
+    void CreateUnit()
+    {
+        unit = new BAIBAI11.BoardCell(cellPrefab, new Vector3(0, 0, 0), 1f);
+        unit.UpdateFace("9", "7");
+    }
+
 
     //4.测试功能:点击按钮时调用动画函数，播放卡片旋转动画
     public void TestRotation() {
+        //adjust camera
+        main.transform.position = new Vector3(0, 0, -100);
+
+        if (ranUnits != null)
+        {
+            ClearRanSpeedCells();
+        }
+
+        if (timeChainUnits != null)
+        {
+            ClearTimeChainCells();
+        }
+
+        if (billUnits != null)
+        {
+            ClearBillBoardCells();
+        }
+
+        if (unit == null) {
+            CreateUnit();
+        }
         if (!rotateTest)
         {
             rotateTest = true;
@@ -74,7 +95,6 @@ public class BillBoardControl : MonoBehaviour
         else {
             rotateTest = false;
         }
-
     }
 
     //5.测试功能:设置文本函数
@@ -83,80 +103,125 @@ public class BillBoardControl : MonoBehaviour
     }
 
 
+
+
     //7.实例化8个B，分别代表小时，分钟，秒，毫秒（为方便制作，我们定义1秒=60毫秒）。
     //实现计时功能，毫秒卡片在飞速旋转变化，秒卡片每隔一秒旋转变化，分钟卡片每隔一分钟旋转变化，小时卡片每隔一小时旋转变化。
-    void CreateTimeCells() {
 
-        //adjust camera
-        main.transform.position = new Vector3(0, 20, -100);
+    void CreateTimeChainCells() {
+
+        // set chain values
+        float[] durationValues = new float[] { 24 * 3600f, 3600f, 600f, 60f, 10f, 1f, 1 / 6f, 1 / 60f };
+        int[] boundValues = new int[] { 24, 10, 6, 10, 6, 10, 6, 10 };
 
         //initiate cells
-        timeUnits = new BAIBAI11.TimeCell[8];
+        timeChainUnits = new BAIBAI11.TimeChainCell[8];
         float w = (BAIBAI11.BoardCell.width + 1);
-        for (int i = 0; i < timeUnits.Length; i++)
-        {
-            timeUnits[i] = new BAIBAI11.TimeCell(cellPrefab, new Vector3((-1 * timeUnits.Length/2) * w + w * i, 30, 0), -0f);
-            timeUnits[i].SetInitalFace();
+        for (int i = 0; i < timeChainUnits.Length; i++) {
+            timeChainUnits[i] = new BAIBAI11.TimeChainCell(cellPrefab, new Vector3((-1 * timeChainUnits.Length / 2) * w + w * i, 30, 0), -0f);
+            timeChainUnits[i].UpdateFace(timeChainUnits[i].TimeValue.ToString(), (timeChainUnits[i].TimeValue + 1).ToString());
+
+            //set duration & bound
+            timeChainUnits[i].Duration = durationValues[i];
+            timeChainUnits[i].UpperBound = boundValues[i];
+
+            //set fwd cell
+            if (i >= 1) {
+                timeChainUnits[i].fwdCell = timeChainUnits[i - 1];
+            }
+
+        }
+    }
+
+    void UpdateTimeChainCells() {
+        for (int i = 0; i < timeChainUnits.Length; i++) {
+            timeChainUnits[i].Checker += Time.deltaTime;
+            timeChainUnits[i].CellRotate();
+            StartCoroutine(timeChainUnits[i].SnapRotation());
         }
 
-        ResetTimer();
+        timeChainUnits[timeChainUnits.Length - 1].rotateOn = true;
+        timeChainUnits[timeChainUnits.Length-1].UpdateDuration();
     }
 
-    void ResetTimer() {
-        timer = 0f;
+    void ClearTimeChainCells() {
+        for (int i = 0; i < timeChainUnits.Length; i++) {
+            timeChainUnits[i].ClearCell();
+        }
+        timeChainUnits = null;
     }
 
 
 
-    void RunTimeCells()
+    //7. 计时功能 测试button
+    public void StartTimer()
     {
+        //adjust camera
+        main.transform.position = new Vector3(0, 20, -150); 
 
-        timer += Time.deltaTime;
-
-        if (timer > 0.016f) {
-            timeUnits[7].SetFlip(true);
-            timer = 0;
-            minSec += 1;
-
+        if(ranUnits != null) {
+            ClearRanSpeedCells();
         }
 
-        if(minSec > 10) {
-            timeUnits[6].SetFlip(true);
-            minSec = 0;
-            tenMinSec += 1;
-        }
-
-        if (tenMinSec > 6)
+        if (unit != null)
         {
-            timeUnits[5].SetFlip(true);
-            tenMinSec = 0;
-            sec += 1;
+            unit.ClearCell();
+            unit = null;
         }
 
-        if (sec > 10)
+        if (billUnits != null)
         {
-            timeUnits[4].SetFlip(true);
-            sec = 0;
-            tenSec += 1;
+            ClearBillBoardCells();
         }
 
+        if (timeChainUnits == null) {
+            CreateTimeChainCells();
+        }
 
-
-        for (int i = 0; i < timeUnits.Length; i++)
+        if (!timerOn)
         {
-            timeUnits[i].Turn();
+            timerOn = true;
         }
+        else {
+            timerOn = false;
+        }
+
     }
+
 
 
     //9.实例化600个C，在屏幕上组成一个大矩形，所有卡面使用随机速度翻转。
+    public void CreateRandom600() {
+
+        if (ranUnits == null) {
+            CreateRandomSpeedCells(20, 30);
+        }
+        else {
+            ClearRanSpeedCells();
+        }
+
+        if(timeChainUnits != null) {
+            ClearTimeChainCells();
+        }
+
+        if (unit != null)
+        {
+            unit.ClearCell();
+            unit = null;
+        }
+
+        if (billUnits != null)
+        {
+            ClearBillBoardCells();
+        }
+    }
 
     void CreateRandomSpeedCells(int row, int col) {
 
         ranUnits = new BAIBAI11.RanSpeedCell[row * col];
 
         //adjust camera according to the size
-        main.transform.position = new Vector3(col, row * 2, -100 * (Mathf.Ceil(Mathf.Log(ranUnits.Length,4f))-1));
+        main.transform.position = new Vector3(col, row * 2, -100 * (Mathf.Ceil(Mathf.Log(ranUnits.Length,4f))-1f));
 
         float w = (BAIBAI11.BoardCell.width + 1);
         float h = BAIBAI11.BoardCell.height;
@@ -176,10 +241,137 @@ public class BillBoardControl : MonoBehaviour
         }
     }
 
+    void ClearRanSpeedCells() {
+        for (int i = 0; i < ranUnits.Length; i++)
+        {
+            ranUnits[i].ClearCell();
+
+        }
+        ranUnits = null;
+    }
+
+
     /*10.实现机场机械广告牌功能。完成如下要求：
     * 如视频展示的那样，部分区域快速翻转。部分区域则维持稳定状态。
     * 可进行微创新，像球场人浪那样，利用时间差，让部分区域展示规律渐进的翻转效果。
     * 自由发挥自己想要的效果。  
      */
-     
+
+    // Button A: create billboarad test
+    public void CreateBillboardTest() {
+        if (billUnits == null) { 
+        CreateBillboardSim(25, 40); }
+
+        if (ranUnits != null)
+        {
+            ClearRanSpeedCells();
+        }
+
+        if (timeChainUnits != null)
+        {
+            ClearTimeChainCells();
+        }
+
+        if (unit != null)
+        {
+            unit.ClearCell();
+            unit = null;
+        }
+    }
+
+    void CreateBillboardSim(int row, int col)
+    {
+
+        billUnits = new BAIBAI11.BillboardSim[row * col];
+
+        //adjust camera according to the size
+        main.transform.position = new Vector3(col, row * 2, -100 * (Mathf.Ceil(Mathf.Log(billUnits.Length, 4f)) - 0.5f));
+
+        float w = (BAIBAI11.BoardCell.width + 1);
+        float h = BAIBAI11.BoardCell.height;
+
+        for (int i = 0; i < billUnits.Length; i++)
+        {
+            billUnits[i] = new BAIBAI11.BillboardSim(cellPrefab, new Vector3(((-1 * col / 2 * w)) + w * (i % col), (-1 * row / 2 * w) + h * (i / col), 0), 1f, Random.Range(0,2));
+
+            if (i % col != 0) {
+                billUnits[i].leftCell = billUnits[i - 1];
+            }
+
+            if(i / col > 0) {
+                billUnits[i].downCell = billUnits[i - col];
+            }
+        }
+
+        BillboardSim.BoardCol = col;
+        BillboardSim.BoardRow = row;
+
+    }
+
+    // test B: wave from right
+    public void WaveEffect(int  waveLen)
+    {
+        waveOn = true;
+
+        for (int i = 0; i < billUnits.Length; i++) {
+
+            billUnits[i].Duration = 1.5f;
+
+            if (i % BillboardSim.BoardCol == BillboardSim.BoardCol - 1)
+            {
+                for (int j=0; j<waveLen; j++) {
+                    billUnits[i-j].rotateOn = true;
+                }
+
+            }
+        }
+
+    }
+
+
+    void RotateBillBoardCells()
+    {
+        for (int i = 0; i < billUnits.Length; i++)
+        {
+            billUnits[i].CellRotate();
+            billUnits[i].Checker += Time.deltaTime;
+        }
+    }
+
+
+    void WaveBillBoardCells()
+    {
+        for (int i = 0; i < billUnits.Length; i++)
+        {
+            billUnits[i].CellRotate(); 
+            billUnits[i].Checker += Time.deltaTime;
+            billUnits[i].Wave();
+        }
+    }
+
+
+
+    // test C: copy value from left
+    public void CopyValuefromLeft() {
+        for (int i = 0; i < billUnits.Length; i++)
+        {
+            if (i % BillboardSim.BoardCol != 0)
+            {
+                billUnits[i].CellValue = billUnits[i].leftCell.CellValue;
+                billUnits[i].UpdateCellValue();
+            }
+        }
+    }
+
+
+    void ClearBillBoardCells()
+    {
+        for (int i = 0; i < billUnits.Length; i++)
+        {
+            billUnits[i].ClearCell();
+
+        }
+        billUnits = null;
+    }
+
 }
--- HomeWork/Lesson11/L11_baibai/Assets/L11HW/Cells.cs
+++ HomeWork/Lesson11/L11_baibai/Assets/L11HW/Cells.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace BAIBAI11 {
+namespace BAIBAI11
+{
 
     //3.编写一个class A，输入为上述prefab和旋转速度，编写一个卡片绕X轴旋转的动画函数。
     public class BoardCell
@@ -17,7 +18,8 @@ namespace BAIBAI11 {
         static public float width = 12f;
         static public float height = 18f;
 
-        public BoardCell(GameObject _prefab, Vector3 _pos, float sp) {
+        public BoardCell(GameObject _prefab, Vector3 _pos, float sp)
+        {
             unitCell = Object.Instantiate(_prefab, _pos, Quaternion.identity);
             currentE = unitCell.transform.eulerAngles;
             speed = sp;
@@ -25,7 +27,8 @@ namespace BAIBAI11 {
             //height = unitCell.transform.GetChild(1).GetComponent<Transform>().localScale.y * 2f;
         }
 
-        public virtual void CellRotate() {
+        public virtual void CellRotate()
+        {
 
             currentE.x += speed * 360f * Time.deltaTime;
             currentE.x = currentE.x % 360f;
@@ -34,105 +37,136 @@ namespace BAIBAI11 {
         }
 
         //5.文本函数，可改变正反两面的文本
-        public void UpdateFace(string face, string back) {
+        public void UpdateFace(string face, string back)
+        {
             unitCell.transform.GetChild(0).GetComponent<TextMesh>().text = back;
             unitCell.transform.GetChild(2).GetComponent<TextMesh>().text = face;
         }
 
-        public virtual void RandomFace() {
+        public virtual void RandomFace()
+        {
             int x = Random.Range(0, 10);
             int y = Random.Range(0, 10);
             Debug.Log("change text: " + x + ", " + y);
             UpdateFace(x.ToString(), y.ToString());
+        }
+
+        public virtual void ClearCell()
+        {
+            Object.Destroy(unitCell);
         }
     }
 
 
 
     //6.编写一个class B继承自A，请自由发挥，为class B赋予这样的功能：每旋转一次，卡片的文本会变化。WIP
-    // 改： 使用statemachine？ flip/ flop/ idle /increment above
-    // 存入text变量存储秒、分钟等
+    // 改：实现是实现了，比较hard code。是不是使用statemachin比较干净？
+    // 是否base的rotate方法写的有问题？不能顺利转到位，这能用了snap的方式。
 
-    public class TimeCell : BoardCell
+    public class TimeChainCell : BoardCell
     {
-        private bool flip = false;
-        private int currentNum;
-        private int nextNum;
 
-        public TimeCell(GameObject _prefab, Vector3 _pos, float sp) : base(_prefab, _pos, sp)
+        float upperBound; // the bound of value
+        float timeValue; // the current value on the frontFace
+        float nextValue; // current value + 1
+        float duration; // a duration is past the cell will rotate and change value
+        float checker; // updated with time
+
+        bool frontFace = true; // if frontFace is facing the camera
+        public bool rotateOn = false;
+
+        public TimeChainCell fwdCell; // the cell before 
+
+        public float TimeValue { get => timeValue; set => timeValue = value; }
+        public float Duration { get => duration; set => duration = value; }
+        public float UpperBound { get => upperBound; set => upperBound = value; }
+        public float Checker { get => checker; set => checker = value; }
+
+        public TimeChainCell(GameObject _prefab, Vector3 _pos, float sp) : base(_prefab, _pos, sp)
         {
+            timeValue = 0f;
+            nextValue = timeValue + 1f;
+        }
+
+        public void UpdateDuration()
+        {
+
+            //CellRotate();
+
+            if (checker >= duration)
+            {
+                //CellRotate();
+                rotateOn = true;
+                timeValue += 1f;
+                checker = 0f;
+
+                if (Mathf.Approximately(nextValue, 0))
+                {
+                    //if (timeValue >= (upperBound-0.1f)) {
+                    timeValue = 0f;
+                    if (fwdCell != null)
+                    {
+                        fwdCell.rotateOn = true;
+                        fwdCell.UpdateDuration();
+                    }
+                }
+
+                nextValue = timeValue + 1;
+                if (Mathf.Approximately(nextValue, upperBound))
+                {
+                    nextValue = 0f;
+                }
+                if (frontFace)
+                {
+                    UpdateFace(nextValue.ToString(), timeValue.ToString());
+                    frontFace = false;
+                }
+                else
+                {
+                    UpdateFace(timeValue.ToString(), nextValue.ToString());
+                    frontFace = true;
+                }
+
+            }
+
+
 
         }
 
         public override void CellRotate()
         {
-            currentE.x += speed * 360f * Time.deltaTime;
-            //currentE.x = currentE.x % 360f;
-
-            if (currentE.x > 360f || currentE.x < -360f)
+            if (rotateOn)
             {
-                currentE.x = currentE.x % 360f;
-                RandomFace();
-            }
-
-
-            unitCell.transform.eulerAngles = currentE;
-            //Debug.Log("unit" + unitCell.transform.eulerAngles);
-        }
-
-        public void SetInitalFace() {
-            currentNum = 0;
-            nextNum = currentNum + 1;
-            UpdateFace(currentNum.ToString(),nextNum.ToString());
-        }
-
-        //public void UpdateFace() {
-        //    currentNum = (nextNum + 1) % 10;
-        //    nextNum = (currentNum + 1) % 10;
-        //    UpdateFace(currentNum.ToString(), nextNum.ToString());
-        //}
-
-        public void SetFlip(bool b) {
-            flip = b;
-        }
-
-        public bool GetFlip() {
-            return flip;
-        }
-
-        public void Turn()
-        {
-            if (flip) {
-                Debug.Log(currentE.x);
-                currentE.x += -2* 360 * Time.deltaTime;
-
-
-
-                if ((currentE.x + 180f) > -5f && (currentE.x + 180f) < 5f ) {
-                    //currentE.x = -184f;
-                    Debug.Log("turning1");
-                    flip = false;
-                    currentNum = (nextNum + 1)%10;
-                    UpdateFace(currentNum.ToString(), nextNum.ToString());
-                }
-
-                if ((currentE.x + 360f) > -5f && (currentE.x + 360f) < 5f )
-                {
-                    //currentE.x = -364f;
-                    Debug.Log("turning2");
-                    flip = false;
-                    nextNum = (currentNum + 1)%10;
-                    UpdateFace(currentNum.ToString(), nextNum.ToString());
-                }
-
-
+                currentE.x += 900f * Time.deltaTime;
                 unitCell.transform.eulerAngles = currentE;
-                currentE.x = currentE.x % 360f;
+                if (Mathf.Approximately(currentE.x % 180f, 0f))
+                {
+                    rotateOn = false;
+
+                }
             }
         }
 
+        public IEnumerator SnapRotation()
+        {
+            if (rotateOn)
+            {
+                yield return new WaitForSeconds(0.1f);
+                if (frontFace)
+                {
+                    currentE.x = 0f;
+                }
+                else
+                {
+                    currentE.x = -180f;
+                }
+                unitCell.transform.eulerAngles = currentE;
+                rotateOn = false;
+            }
 
+        }
     }
+
 
     //8.编写一个class C继承自A，请自由发挥，为class C赋予这样的功能：每旋转一次，卡片的文本会自加一次。（假设文本只能显示数字和字母）
     public class RanSpeedCell : BoardCell
@@ -165,6 +199,102 @@ namespace BAIBAI11 {
             Debug.Log("change text: " + x + ", " + y);
             UpdateFace(textDisplay[x].ToString(), textDisplay[y].ToString());
         }
+    }
+
+
+    //10.billboard
+    public class BillboardSim : BoardCell
+    {
+        static int boardRow;
+        static int boardCol;
+
+        static string textDisplay = "ABCDEFGHIJKOPQRSTUVWXYZ";
+        static string digitDisplay = "0123456789";
+        string cellValue;
+        float duration;
+        float checker;
+
+        public BillboardSim leftCell;
+        public BillboardSim downCell;
+
+        bool frontFace = true; // if frontFace is facing the camera
+        public bool rotateOn = false;
+
+        public static int BoardRow { get => boardRow; set => boardRow = value; }
+        public static int BoardCol { get => boardCol; set => boardCol = value; }
+        public string CellValue { get => cellValue; set => cellValue = value; }
+        public float Duration { get => duration; set => duration = value; }
+        public float Checker { get => checker; set => checker = value; }
+
+        public BillboardSim(GameObject _prefab, Vector3 _pos, float sp, int isText) : base(_prefab, _pos, sp)
+        {
+            if (isText == 0)
+            {
+                cellValue = digitDisplay[Random.Range(0, digitDisplay.Length)].ToString();
+                speed = 4f;
+            }
+            else
+            {
+                cellValue = textDisplay[Random.Range(0, textDisplay.Length)].ToString();
+                speed = 1f;
+            }
+
+            UpdateFace(cellValue, cellValue);
+            //rotateOn = true;
+            checker = 0f;
+
+        }
+
+        public void UpdateCellValue()
+        {
+            UpdateFace(cellValue, cellValue);
+        }
+
+        public override void CellRotate()
+        {
+            if (rotateOn)
+            {
+
+                currentE.x += speed * 360f * Time.deltaTime;
+                currentE.x = currentE.x % 360f;
+                unitCell.transform.eulerAngles = currentE;
+
+            }
+        }
+
+
+        public void Wave()
+        {
+
+            if (rotateOn == true && checker >= duration)
+            {
+
+                if (leftCell != null && leftCell.rotateOn == false)
+                {
+                    leftCell.rotateOn = true;
+                    leftCell.checker = 0f;
+                    leftCell.Wave();
+                }
+
+
+
+                if ((currentE.x%180f) >= (currentE.x % 360f))
+                {
+                    currentE.x = 180f;
+                }
+                else
+                {
+                    currentE.x = 360f;
+                }
+
+                unitCell.transform.eulerAngles = currentE; 
+                rotateOn = false;
+
+                
+            }
+
+        }
+
     }
 
 }
