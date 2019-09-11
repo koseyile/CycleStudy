8cab87e268cea5c0e85f472fbb6dee521f5289cd
melfless <mengm@gmail.com>
2019-09-11 20:48:40 +0800

    Lesson11MengMeng
    修复了unity版本不同步导致UI组件引用丢失的问题
    以及做了一下附加题

--- HomeWork/Lesson11/Lesson11_MengMeng/Scripts/CardManager.cs
+++ HomeWork/Lesson11/Lesson11_MengMeng/Scripts/CardManager.cs
@@ -5,18 +5,22 @@ namespace MengMeng
 {
     public class CardManager : MonoBehaviour
     {
+        CardController_V3[,] Cards;
+        int Min_x, Min_y, Max_x, Max_y;
+        float TimeCount_Q10;
+        bool Q10_State = true;
         // Start is called before the first frame update
         void Start()
         {
             CreateCard_A();
             Question_7();
-            Question_9();
+            Cards = Question_9();
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            Question_10();
         }
 
         void CreateCard_A()
@@ -45,23 +49,66 @@ namespace MengMeng
             CreateCard_B(3600, new Vector3(-20, 40, 0));
             CreateCard_B(3600, new Vector3(-30, 40, 0));
         }
-        void CreateCard_C(float _RotateInterval, Vector3 _pos, float scale = 0.4f)
+        CardController_V3 CreateCard_C(float _RotateInterval, Vector3 _pos, float scale = 0.3f)
         {
             GameObject Card = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Card"), this.transform);
             Card.transform.localPosition = _pos;
             Card.transform.localScale = new Vector3(scale, scale, scale);
             CardController_V3 CardController = Card.AddComponent<CardController_V3>();
             CardController.RotateInterval = _RotateInterval;
+            return CardController;
         }
-        void Question_9()
+        CardController_V3[,] Question_9()
         {
-            for(int i = 0; i< 30;i++)
+            CardController_V3[,] Cards = new CardController_V3[40, 15];
+            for (int i = 0; i < Cards.GetLength(0); i++)
             {
-                for(int j = 0; j< 20;j++)
+                for (int j = 0; j < Cards.GetLength(1); j++)
                 {
-                    CreateCard_C(Random.Range(0f, 10f), new Vector3(-90 + i * 6, 20 - j * 9, 0));
+                    Cards[i, j] = CreateCard_C(Random.Range(2f, 5f), new Vector3(-90 + i * 4, 20 - j * 6, 0));
                 }
             }
+            return Cards;
+        }
+
+        void RotateCards( bool bActive = true)
+        {
+            if (bActive)
+            {
+                int X_Length = 8, Y_Length = 6;
+                Min_x = Random.Range(0, Cards.GetLength(0) - X_Length);
+                Min_y = Random.Range(0, Cards.GetLength(1) - Y_Length);
+                Max_x = Min_x + X_Length;
+                Max_y = Min_y + Y_Length;
+            }
+            for (int i = Min_x; i < Max_x; i++)
+            {
+                for (int j = Min_y; j < Max_y; j++)
+                {
+                    CardController_V3 card = Cards[i, j];
+                    if (bActive)
+                    {
+                        card.RotateInterval = 0.1f;
+                    }
+                    else
+                    {
+                        card.RotateInterval = Random.Range(2f, 5f);
+                    }
+                }
+            }
+        }
+
+        void Question_10()
+        {
+            TimeCount_Q10 += Time.deltaTime;
+            if(TimeCount_Q10 > 3)
+            {
+                TimeCount_Q10 = 0;
+                RotateCards(Q10_State);
+                Q10_State = !Q10_State;
+            }
+           
+            
         }
     }
 
