ca0d3a3665d6ac42cf14d7d6f96923879689855d
w <756424501@qq.com>
2019-09-23 00:07:22 +0800

    homework

---
+++ HomeWork/Lesson12/Wupeng/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Wupeng
+{
+    public class UI_Manager
+    {
+
+        public float m_Height;
+
+        public float m_Speed = 6000.0f;
+
+        static UI_Manager _current;
+
+        static public UI_Manager Current
+        {
+
+            get
+            {
+                if (_current != null) { return _current; }
+
+                return new UI_Manager();
+            }
+        }
+
+        public UI_Manager()
+        {
+            this.m_Height = Screen.height - 100;
+        }
+    }
+
+}
