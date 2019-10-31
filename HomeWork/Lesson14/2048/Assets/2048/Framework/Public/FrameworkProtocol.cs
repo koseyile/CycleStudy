79629c0c9058bf0c8d3c61c642438d4b1d0f6cd8
melfless <melfless@foxmail.com>
2019-10-31 23:30:14 +0800

    输入协议修改_mengmeng

--- HomeWork/Lesson14/2048/Assets/2048/Framework/Public/FrameworkProtocol.cs
+++ HomeWork/Lesson14/2048/Assets/2048/Framework/Public/FrameworkProtocol.cs
@@ -9,6 +9,10 @@ namespace Game2048Framework
     {
         None,
         MoveRight,   //zcy:当玩家触发键盘右键抬起事件时，IGameInput.GetInputData必须返回此值
+        MoveLeft,   //mengmeng:当玩家触发键盘左键抬起事件时，IGameInput.GetInputData必须返回此值
+        MoveUp,   //mengmeng:当玩家触发键盘上键抬起事件时，IGameInput.GetInputData必须返回此值
+        MoveDown,   //mengmeng:当玩家触发键盘下键抬起事件时，IGameInput.GetInputData必须返回此值
+        Click,    //mengmeng:当玩家触发点击事件时，IGameInput.GetInputData必须返回此值
     }
     //------------------------------------------------------------Input protocol end
 
