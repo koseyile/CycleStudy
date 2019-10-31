4304f2154b9c5fa0faa4e2fbc84aba95f4a89513
melfless <melfless@foxmail.com>
2019-11-01 00:28:14 +0800

    mengmeng_输入模块实现
    由于跑不起来无法验证，所以我也不知道写得对不对，有没有bug

--- HomeWork/Lesson14/2048/Assets/2048/Framework/Public/FrameworkProtocol.cs
+++ HomeWork/Lesson14/2048/Assets/2048/Framework/Public/FrameworkProtocol.cs
@@ -12,7 +12,7 @@ namespace Game2048Framework
         MoveLeft,   //mengmeng:当玩家触发键盘左键抬起事件时，IGameInput.GetInputData必须返回此值
         MoveUp,   //mengmeng:当玩家触发键盘上键抬起事件时，IGameInput.GetInputData必须返回此值
         MoveDown,   //mengmeng:当玩家触发键盘下键抬起事件时，IGameInput.GetInputData必须返回此值
-        Click,    //mengmeng:当玩家触发点击事件时，IGameInput.GetInputData必须返回此值
+        Click,    //mengmeng:当玩家触发鼠标左键抬起事件时，IGameInput.GetInputData必须返回此值
     }
     //------------------------------------------------------------Input protocol end
 
