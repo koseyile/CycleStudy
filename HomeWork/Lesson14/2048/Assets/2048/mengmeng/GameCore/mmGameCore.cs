e05a9c0a933bc12f37b1ad307ecbd6dc4d5b514c
melfless <melfless@foxmail.com>
2019-11-11 23:43:54 +0800

    GameCore提交

---
+++ HomeWork/Lesson14/2048/Assets/2048/mengmeng/GameCore/mmGameCore.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game2048Framework;
+
+namespace mm
+{
+    public class mmGameCore : IGameCore
+    {
+        private IGameInput gameInput;
+        private IGameRender gameRender;
+        private ICheckBoardObject checkBoardObject;
+        private static float MoveSpeed = 3f;
+
+        public void ModuleInit()
+        {
+            gameInput = GameFramework.singleton.getInput();
+            gameRender = GameFramework.singleton.getGameRender();
+
+            //   gameRender.CreateObject(RenderProtocol.CreateBackImgObject);
+            checkBoardObject = gameRender.CreateObject(RenderProtocol.CreateCheckBoardObject) as ICheckBoardObject;
+
+        }
+
+        public void ModuleUpdate()
+        {
+
+        }
+
+        public void ModuleDestroy() { }
+
+        private INumberObject CreateNumber(int Num, Vector2 pos)
+        {
+            INumberObject numberObject = gameRender.CreateObject(RenderProtocol.CreateNumberObject) as INumberObject;
+            numberObject.SetPosition(pos);
+            numberObject.SetText(Num);
+            numberObject.SetColor(Color.black);
+            return numberObject;
+        }
+        private void MoveNumber(INumberObject numberObject, Vector2 pos)
+        {
+            if (numberObject.GetPosition() != pos)
+            {
+                numberObject.SetPosition(Vector2.MoveTowards(numberObject.GetPosition(), pos, MoveSpeed));
+            }
+        }
+        private void DestoryNumber(INumberObject numberObject)
+        {
+            GameFramework.singleton.getGameRender().DestroyObject(numberObject);
+        }
+        private void MergeNumber(INumberObject numberObjectA, INumberObject numberObjectB)
+        {
+            numberObjectB.SetText(numberObjectB.GetNumber() + numberObjectA.GetNumber());
+            DestoryNumber(numberObjectA);
+        }
+    }
+
+}
