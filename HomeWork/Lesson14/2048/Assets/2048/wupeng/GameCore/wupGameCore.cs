adf8854a5caf6a5485bc68e62a003b5af65b35de
GravityMa <756424501@qq.com>
2019-11-16 22:49:19 +0800

    Update wupGameCore.cs

--- HomeWork/Lesson14/2048/Assets/2048/wupeng/GameCore/wupGameCore.cs
+++ HomeWork/Lesson14/2048/Assets/2048/wupeng/GameCore/wupGameCore.cs
@@ -22,15 +22,15 @@ namespace WP
 
     public class wupGameCore : IGameCore
     {
-        private int gameSize = 4;
+        private int   gameSize = 4;
         private float speed_move = 0.5f;
+        private float waitTime = 0.5f;
 
         private GameState gameState;
         private UserState userState;
-        private int score;
 
         private INumberObject[,] numbers;
-        private Vector2[,] blank;
+        private List<Vector2> blank;
 
         public void ModuleInit()
         {
@@ -38,7 +38,7 @@ namespace WP
             userState = UserState.None;
             numbers = new INumberObject[gameSize, gameSize];
 
-
+            Begin();
         }
 
         public void ModuleUpdate()
@@ -58,62 +58,87 @@ namespace WP
 
         }
 
+        //游戏逻辑
+        public void GamePlay()
+        {
+            switch (userState)
+            {
+                case UserState.Wait:
+                    if (waitTime > 0.0f)
+                    {
+                        waitTime -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        userState = UserState.PlayerInput;
+                        waitTime = 0.5f;
+                    }
+                    break;
+                case UserState.PlayerInput:
+                    PlayerMove();
+
+
+
+                    break;
+            }
+
+        }
+
         public void Begin()
         {
+            gameState = GameState.Gaming;
+
             for (int i = 0; i < gameSize; i++)
             {
                 for (int j = 0; j < gameSize; j++)
                 {
                     numbers[i, j] = GameFramework.singleton.getGameRender().CreateObject(RenderProtocol.CreateNumberObject) as INumberObject;
-                    blank[i, j] = new Vector2(i, j);
+                    numbers[i, j].SetNumber(0);
+                    numbers[i, j].SetPosition(new Vector2(i, j));
+
+                    blank.Add(new Vector2(i, j));
                 }
             }
 
-            Vector2 first = new Vector2(Random.Range(0, 4), Random.Range(0, 4));
-            Vector2 second = new Vector2(Random.Range(0, 4), Random.Range(0, 4)); 
+            GenarateRandomNumbers();
 
-            while (second == first)
-            {
-                second = new Vector2(Random.Range(0, 4), Random.Range(0, 4));
+            userState = UserState.PlayerInput;
+        }
+
+        public void GenarateRandomNumbers()
+        {
+            int[] nums = { 2, 4 };
+
+            for (int i = 0; i < 2; i ++)
+            {            
+                int index = Random.Range(0, blank.Count);
+
+                if (index >= 0)
+                {
+                    Vector2 first = blank[index];
+                    blank.RemoveAt(index);
+
+
+                    INumberObject number = GameFramework.singleton.getGameRender().CreateObject(RenderProtocol.CreateNumberObject) as INumberObject;
+
+                    number.SetNumber(nums[Random.Range(0, 1)]);
+                    number.SetPosition(first);
+                }
+                else
+                {
+                    Debug.Log("There is no space in checkboard! Can't genarate numbers!");
+                }
             }
-
-
-                
-
         }
-
-
-        public void FillBlank(Vector2)
-        {
-
-        }
-
-
-        //游戏逻辑
-        public void GamePlay()
-        {
-
-        }
-
-
-
 
         public void CreateNumber(int number, Vector2 index)
         {
-            //在棋盘特定位置创建特定数字
             INumberObject numberOb = GameFramework.singleton.getGameRender().CreateObject(RenderProtocol.CreateNumberObject) as INumberObject;
             numberOb.SetPosition(index);
             numberOb.SetNumber(number);
         }
 
-        public void ClearNumber(int[,] index)
-        {
-            //清理numbers上特定
-
-        }
-
-        //移动数字
-        public void Move()
+        public void PlayerMove()
         {
             switch (GameFramework.singleton.getInput().GetInputData())
             {
@@ -131,16 +156,16 @@ namespace WP
                     break;
                 default:
                     break;
-
             }
 
         }
 
         public void NumbersUp()
         {
-            //判断，更新数组
 
-            //调用Render模块，更新显示
+
+
+
         }
 
         public void NumbersDown()
@@ -148,14 +173,14 @@ namespace WP
 
         }
 
-        public void NumbersRight()
+        public void NumbersRight() //在此帧中，需要对所有的Number进行计算，并完成动画函数的调用
         {
-            //伪代码
-            for ()
+            for (int i = 0; i < gameSize; i ++)
             {
-                Move();
-                Merge();
-                Move();
+                for (int j = gameSize - 1; j >= 0; j -- )
+                {
+
+                }
             }
         }
 
@@ -226,11 +251,6 @@ namespace WP
             return true;
         }
 
-        public float Distance(Vector2 v1, Vector2 v2)
-        {
-            Vector2 v3 = v1 - v2;
-            return Mathf.Sqrt(v3.x * v3.x + v3.y + v3.y);
-        }
     }
 
 }
